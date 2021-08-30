7ece1940298d5d5795099eea9a0f2d5c06940ced
qian <ph.Milaya@gmail.com>
2021-08-30 19:53:33 +0800

    ...

--- vue.config.js
+++ vue.config.js
@@ -22,11 +22,10 @@ module.exports = {
         args[0].isProd = true
         return args
       })
-
     })
+
     config.when(process.env.NODE_ENV === 'development', config => {
       config.entry('app').clear().add('./src/main-dev.ts')
-
       config.plugin('html').tap(args => {
         args[0].isProd = false
         return args
